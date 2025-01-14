9666d97b3a73382b1b297da7c93e3acc1ec1d66b
ioi_yassine <yassineaboutaib11@gmail.com>
2025-01-15 00:15:16 +0100

    Update App.js
    Adding create pin page router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 import Home from "./pages/Home";
+import Create from "./pages/Create";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}>
+        <Route>
           <Route index element={<Home />} />
           <Route path="home" element={<Home />} />
+          <Route path="create" element={<Create />} />
         </Route>
       </Routes>
     </BrowserRouter>
